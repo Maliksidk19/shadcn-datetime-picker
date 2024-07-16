bfd767f550de9289816dcfe9c2a1719250f2b48c
Muhammad Saad <malik.saadking19@yahoo.com>
2024-07-16 14:04:55 +0500

    repo link added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 import { DatetimePickerV1 } from "@/components/date-time-picker-v1";
 import { DateTimePickerV2 } from "@/components/date-time-picker-v2";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <section className="flex-1 m-20 flex flex-col gap-10 w-[400px]">
-      <DatetimePickerV1 />
-      <DateTimePickerV2 />
-    </section>
+    <div>
+      <span className="absolute flex items-center gap-2 top-10 right-10">
+        <GitHubLogoIcon className="h-6 w-6" />{" "}
+        <Link
+          href="https://github.com/Maliksidk19/shadcn-datetime-picker"
+          target="_blank"
+        >
+          Datetime Picker Repo
+        </Link>
+      </span>
+      <section className="flex-1 m-20 flex flex-col gap-10 w-[400px]">
+        <DatetimePickerV1 />
+        <DateTimePickerV2 />
+      </section>
+    </div>
   );
 };
 
